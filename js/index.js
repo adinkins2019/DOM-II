89caa355368abcf446e4485b24e269aacc9e9c53
adinkins2019 <afdinki@gmail.com>
2019-08-10 13:49:34 -0400

    Modified index.less index.css and index.js files

--- js/index.js
+++ js/index.js
@@ -1,1 +1,40 @@
 // Your code goes here
+const images = document.getElementsByTagName('img');
+for(let i = 0; i < images.length; i++) {
+    images[i].addEventListener('click', (event) => {
+        event.target.style.transform = "scale(1.1)";
+    });
+    images[i].addEventListener('mouseover', (event) => {
+        event.target.style.transform = "scale(1.1)";
+    });
+    images[i].addEventListener('mouseout', (event) => {
+        event.target.style.transform = "scale(1.0)";
+    });
+}
+
+const btns = document.querySelectorAll('.btn');
+btns.forEach(btn => {
+  btn.addEventListener( 'click', (event) => {
+    event.target.style.backgroundColor = "#17A2B8";
+    event.target.style.color = "white";
+    }
+  );
+  //btn.addEventListener('mouseover')
+}
+
+);
+
+const destinations = document.querySelectorAll('.destination');
+destinations.forEach(destination => {
+    destination.addEventListener('mouseenter', (event) => {
+        event.target.style.backgroundColor = "#17A2B8";
+    });
+    destination.addEventListener('mouseleave', (event) => {
+        event.target.style.backgroundColor = "white";
+    });
+});
+
+/*const mainContent = document.querySelector('.container .home');
+mainContent.addEventListener('scroll', (event) => {
+    event.target.
+})*/
